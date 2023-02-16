b56ca7f1912f6df9fc947694a023799da0e15bd7
rubberhead <bjchenzhengyi@hotmail.com>
2023-02-16 19:49:22 +0000

    Finished `mqpv`

--- coursework/sched-adv.cpp
+++ coursework/sched-adv.cpp
@@ -11,62 +11,54 @@
 #include <infos/util/list.h>
 #include <infos/util/lock.h>
 
-/* Initial priority values for each priority level */
-#define REALTIME_PRIO_BASE_VAL     63; 
-#define INTERACTIVE_PRIO_BASE_VAL 127; 
-#define NORMAL_PRIO_BASE_VAL      191; 
-#define DAEMON_PRIO_BASE_VAL      255; 
-
-/* Priority value increment/decrement deltas for each priority level */
-#define REALTIME_PRIO_DELTA    8; 
-#define INTERACTIVE_PRIO_DELTA 4;
-#define NORMAL_PRIO_DELTA      2; 
-#define DAEMON_PRIO_DELTA      1;
-
 using namespace infos::kernel; 
 using namespace infos::util; 
 
+/* Initial priority values for each priority level */
+constexpr uint8_t PRIO_BASE_VAL[4]    = {50, 100, 150, 200}; 
+
+/* Priority value increment/decrement deltas for each priority level */
+constexpr uint8_t PRIO_DELTA_TABLE[4] = {25, 10, 5, 1}; 
+
+/**
+ * @brief 
+ * Entry for a runqueue. Wraps around a given `entity` to provide "Priority Value" metric.
+ * 
+ * @details
+ * I was inspired by [SGG] Chapter 5.3.4's Priority Scheduling -- for which SJF is a special case. 
+ * This works nothing like SJF though... The "priority value" captures the notion of wait time, as 
+ * it correlates to the number of time slices waited by each process, weighted by PRIO_BASE_VAL (which 
+ * applies to tasks running < quantum) and PRIO_DELTA_TABLE (which applies to tasks running > quantum). 
+ * 
+ * All these should make it *technically* not an algorithm covered in lecture.
+ */
 struct RunqueueEntry 
 {
 public: 
     SchedulingEntity* entity; 
     uint8_t priority_value; 
 
-    /* [DONTUSE] Should be `delete` but `list.h` complains otherwise. */
     RunqueueEntry()
     {
         this->entity = NULL; 
-        this->priority_value = 255;
-        this->_priority_delta = 0; 
+        this->priority_value = __UINT8_MAX__;
+        this->_prio_incr_delta = 0; 
+        this->_prio_decr_delta = 0; 
     } 
-
-    /*
-     * [DONTUSE] Should be `delete` but `list.h` complains otherwise. 
-     * This (imo) is due to `list.h` using copy constructors for its element generic type. 
-    */
     RunqueueEntry(const RunqueueEntry&) = default; 
     RunqueueEntry(RunqueueEntry&&) = default;
     RunqueueEntry(SchedulingEntity* entity) 
     {
         this->entity = entity; 
-        switch (entity->priority()) {
-            case SchedulingEntityPriority::REALTIME:
-                this->priority_value  = REALTIME_PRIO_BASE_VAL;
-                this->_priority_delta = REALTIME_PRIO_DELTA;  
-                break;
-            case SchedulingEntityPriority::INTERACTIVE: 
-                this->priority_value  = INTERACTIVE_PRIO_BASE_VAL;
-                this->_priority_delta = INTERACTIVE_PRIO_DELTA;  
-                break; 
-            case SchedulingEntityPriority::NORMAL: 
-                this->priority_value  = NORMAL_PRIO_BASE_VAL; 
-                this->_priority_delta = NORMAL_PRIO_DELTA; 
-                break; 
-            case SchedulingEntityPriority::DAEMON:
-                this->priority_value  = DAEMON_PRIO_BASE_VAL; 
-                this->_priority_delta = DAEMON_PRIO_DELTA; 
-                break; 
-        }
+        size_t idx = entity->priority(); 
+        this->priority_value   = PRIO_BASE_VAL[idx]; 
+        /*
+         *    Higher priority 
+         * => Faster P-val decrement, slower P-val increment, vice versa
+         * => Preference towards high-priority tasks.
+        */
+        this->_prio_incr_delta = PRIO_DELTA_TABLE[3 - idx]; 
+        this->_prio_decr_delta = PRIO_DELTA_TABLE[idx]; 
     }
 
     /**
@@ -76,10 +68,10 @@ public:
      */
     uint8_t decrement() 
     {
-        if (this->_priority_delta >= this->priority_value) {
+        if (this->_prio_decr_delta >= this->priority_value) {
             this->priority_value = 0; 
         } else {
-            this->priority_value -= this->_priority_delta; 
+            this->priority_value -= this->_prio_decr_delta; 
         }
         return this->priority_value; 
     }
@@ -91,10 +83,10 @@ public:
      */
     uint8_t increment() 
     {
-        if (__UINT8_MAX__ - this->priority_value <= this->_priority_delta) {
+        if (__UINT8_MAX__ - this->priority_value <= this->_prio_incr_delta) {
             this->priority_value = __UINT8_MAX__; 
         } else {
-            this->priority_value += this->_priority_delta; 
+            this->priority_value += this->_prio_incr_delta; 
         }
         return this->priority_value; 
     }
@@ -119,13 +111,22 @@ public:
     
 
 private: 
-    uint8_t _priority_delta; // [FIXME] to const... somehow
+    uint8_t _prio_incr_delta, _prio_decr_delta; 
 }; 
+
+/*
+ * > Why not Map? 
+ * 1. I forgot its existence.
+ * 2. I took a look at its header file and found that it doesn't provide a `remove` impl?! 
+ *    This is a pro-memory-leak operating system. 🫡
+*/
 typedef List<RunqueueEntry> RunQueue; 
 
 /**
- * @brief The Multi-Queue Priority-Value Scheduler.
+ * @brief 
+ * The Multi-Queue Priority-Value Scheduler.
  * 
+ * @details
  * This scheduler emulates the behavior of Multilevel Feedback Queue algorithm, 
  * but preserves each task's priority level and utilizes a more fine-grained control 
  * over each task's priority value -- a variable dependent on (1) its priority level, 
@@ -147,7 +148,7 @@ public:
      */
     void init()
     {
-        // Nothing...
+        // Crickets!
     }
 
     /**
@@ -183,7 +184,7 @@ public:
                 break; 
             }
         }
-        assert(corresponding_entry != NULL); 
+        // assert(corresponding_entry != NULL); 
         runqueues[idx].remove(*corresponding_entry); 
     }
 
@@ -196,11 +197,14 @@ public:
      */
     SchedulingEntity *pick_next_entity() override 
     {
+        // Rant: 
+        // This implementation is full of copy vs. move shenanigans. 
         // Move semantics DOES NOT IMPLY that the memory addresses of l-rvalue would be the same!!!!!!!!!
         // This is (I guess?) due to VA-PA abstraction, which does that job for you (diff VA -> same PA, first now has no ownership). 
         // Cost differential from copy semantics comes from diff VA -> diff PA in comparison. 
-        // (I guess this course really is a little useful for you? Yes to internal knowledge, no to premature optimization)
+        // (I guess this course really is a little useful for me... Yes to internal knowledge, no to premature optimization)
 
+        // Stores *copies* of runqueue elements
         RunqueueEntry firsts[4];
 
         // Iterate over each rq to fill `firsts`
@@ -230,11 +234,20 @@ public:
             _last_selected_ptrs[i] = firsts[i].entity; 
         }
         
-        // This can ptr into firsts no problem, but any alteration needs to be done on runqueue.
+        // This can ptr into firsts but not runqueue -- any alteration needs to be done on runqueue.
         const RunqueueEntry* scheduled_entry_ptr = &firsts[0]; 
 
         // Iterate over firsts, select min or return NULL (if all placeholders)
         for (const auto& entry : firsts) {
+            sched_log.messagef(
+                LogLevel::DEBUG, 
+                "[%s] Found entry {@ 0x%x | P-lvl: %d, P-val: %d}", 
+                name(), 
+                entry.entity, 
+                (entry.entity == NULL) ? -1 : entry.entity->priority(), 
+                entry.priority_value
+            ); 
+
             if (entry.is_placeholder()) {
                 continue;
             }
@@ -255,17 +268,19 @@ public:
                 /* 
                  * [FIXME] O(n) time LMAO, hope I could use ptr but first/last returns const* and 
                  * "list" is really a queue (not even a doubly-linked list!). 
+                 * 
+                 * Selected, on the other hand, should always be at top by RR so... O(1) in practice? 
                 */
                 rq.remove(entry); // see overloaded `==` operator.
-                rq.push(entry); // Back to previous location
+                rq.push(entry);   // Back to previous location
             }
         }
 
         // unwrap
-        assert(scheduled_entry_ptr != NULL);
-        assert(!scheduled_entry_ptr->is_placeholder()); 
+        // assert(scheduled_entry_ptr != NULL);
+        // assert(!scheduled_entry_ptr->is_placeholder()); 
         sched_log.messagef(
-            LogLevel::DEBUG, 
+            LogLevel::INFO, 
             "[%s] Selected entity {@ 0x%x | P-lvl: %d, P-val: %d}", 
             name(), 
             scheduled_entry_ptr->entity, 
